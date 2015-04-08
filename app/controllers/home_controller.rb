cdae4ab94813be1840d239649294669cc716f1ee
batrlatom <tomas.batrla@gmail.com>
2015-04-08 11:58:23 +0200

    Update background

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -43,7 +43,7 @@ logger.info("mail info success")
               referring_member = list_members['data'].detect { |member| member['merges']['RCODE'] == "#{@ref}" }
 logger.info("memberfound : #{email}")
               new_count        = referring_member['merges']['RCOUNT']
-		new_count += 1	
+		new_count = #{new_count} + 1	
 
 logger.info("membercount:  : #{new_count}")
               mailchimp.lists.update_member("#{MAILCHIMP_LIST_ID}", {'euid' => "#{referring_member['id']}"}, 'RCOUNT' => "#{new_count}")
